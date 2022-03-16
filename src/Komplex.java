c6bf8a5cda4997f7298c3095cfc803baffaca9f9
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2022-03-16 13:39:25 +0100

    Initial commit

---
+++ src/Komplex.java
@@ -0,0 +1,21 @@
+public class Komplex {
+    double re;
+    double im;
+
+    public Komplex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public Komplex add(Komplex b) {
+        return b.re + b.re;
+    }
+
+    public Komplex multiply(Komplex b) {
+
+    }
+
+    public double arg() {
+        return 0;
+    }
+}
