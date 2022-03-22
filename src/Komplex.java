d87a0fa6c40fae49667c6e1d4fddcd7488afa099
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2022-03-22 15:18:36 +0100

    Add and multiply application

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Komplex {
     double re;
     double im;
@@ -7,12 +9,25 @@ public class Komplex {
         this.im = im;
     }
 
+    @Override
+    public String toString() {
+        return "Komplex{" +
+                "re=" + re +
+                ", im=" + im + 'i' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(re, im);
+    }
+
     public Komplex add(Komplex b) {
-        return b.re + b.re;
+        return new Komplex(re + b.re, im + b.im);
     }
 
     public Komplex multiply(Komplex b) {
-
+        return new Komplex(re * b.re - im * b.im, im * b.re + re * b.im);
     }
 
     public double arg() {
--- src/KomplexTest.java
+++ src/KomplexTest.java
@@ -15,8 +15,11 @@ public class KomplexTest {
         Komplex c = a.add(b);
         Komplex d = new Komplex(7,3);
 
+        c = c.toString();
+        d = d.toString();
+
         System.out.println(c);
-        if (c.equals(d)) {
+        if (c.equals(d)){
             System.out.println("add funkar!!");
         }
 
